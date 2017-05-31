6f8444b54f37d4ef793ad09eeea371f6a0440152
Peter <peter.otoole14@gmail.com>
2017-05-31 22:49:37 +0100

    got tool shell running

--- index.js
+++ index.js
@@ -5,24 +5,35 @@
  * @created 28 May 2017
  */
 
-const utils = require( "./lib/utils" )
+const utils = require("./lib/utils")
+const helpers = require("./lib/helpers")
+const log = utils.log
 
 // Create exception handler 
-const handleUncaughtExceptions = require( "./lib/uncaught_exception_handler" )
-handleUncaughtExceptions.attachListener( utils.constants.too_name )
+const handleUncaughtExceptions = require("./lib/uncaught_exception_handler")
+handleUncaughtExceptions.attachListener(utils.constants.too_name)
 
 // Create interface
-const interface = require( "./interface" )()
+const interface = require("./lib/interface")()
 const cli = interface.cli
 
 // Set CLI interface for the exception handler and add the logging level
-handleUncaughtExceptions.attachCli( interface.interface )
-utils.setLoggingLevel( cli.v )
+handleUncaughtExceptions.attachCli(interface.interface)
+utils.setLoggingLevel(cli.v)
+
+// Print start
+helpers.printStart()
 
 // Validate input parameters
-const validate = require( "./lib/validate" )
-let configuration = validate( cli )
+const validate = require("./lib/validate")
+let configuration = validate(cli)
 
 // Call main code to rename files in selected folder
-const rename_files = require( "./lib/rename_files" )
-rename_files( configuration.path, configuration.pattern, configuration.selectedTimestamp )
+const rename_files = require("./lib/rename_files")
+rename_files(configuration.path, configuration.pattern, configuration.selectedTimestamp, (error, result) => {
+
+    log.info("Index file done")
+
+    // Print end
+    helpers.printEnd()
+});
