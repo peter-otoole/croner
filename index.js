83b08ad945f791516be20b41490616c1d9733729
Peter <peter.otoole14@gmail.com>
2017-06-11 22:13:40 +0100

    Modified to use exif reader

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ let configuration = validate(cli)
 
 // Call main code to rename files in selected folder
 const rename_files = require("./lib/rename_files")
-rename_files(configuration.path, configuration.pattern, configuration.selectedTimestamp, (error, result) => {
+rename_files(configuration.path, configuration.pattern, (error, result) => {
 
     log.info("Index file done")
 
