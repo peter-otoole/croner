c83b0901a6d0e908360aecf3a427d58f1c4ba87d
Peter O'Toole <peter.otoole14@gmail.com>
2018-09-23 08:31:32 -0500

    Refactored all logic files. Added eslint json file

--- croner.js
+++ croner.js
@@ -3,34 +3,34 @@
  * @author peter-otoole
  */
 
-const utils = require("./lib/utils")
-const helpers = require("./lib/helpers")
-const log = utils.log
+const utils = require('./lib/utils');
+const helpers = require('./lib/helpers');
+const log = utils.log;
 
 // Create exception handler
-const handleUncaughtExceptions = require("./lib/uncaught_exception_handler")
-handleUncaughtExceptions.attachListener(utils.constants.tool_name)
+const handleUncaughtExceptions = require('./lib/uncaught_exception_handler');
+handleUncaughtExceptions.attachListener(utils.constants.tool_name);
 
 // Create interface
-const interface = require("./lib/interface")()
-const cli = interface.cli
+const interface = require('./lib/interface')();
+const cli = interface.cli;
 
 // Set CLI interface for the exception handler and add the logging level
-handleUncaughtExceptions.attachCli(interface.interface)
-utils.setLoggingLevel(cli.v)
+handleUncaughtExceptions.attachCli(interface.interface);
+utils.setLoggingLevel(cli.v);
 
 // Print start
-helpers.printStart()
+helpers.printStart();
 
 // Validate input parameters
-const validate = require("./lib/validate")
-let configuration = validate(cli)
+const validate = require('./lib/validate');
+let configuration = validate(cli);
 
 // Call main code to rename files in selected folder
-const rename_files = require("./lib/rename_files")
-rename_files(configuration.ignoreErrors, configuration.timestamp, configuration.path, configuration.pattern, () => {
-    log.info("Finished renaming files")
+const renameFiles = require('./lib/rename_files');
+renameFiles(configuration.ignoreErrors, configuration.timestamp, configuration.path, configuration.pattern, () => {
+  log.info('Finished renaming files');
 
-    // Print end
-    helpers.printEnd()
+  // Print end
+  helpers.printEnd();
 });
